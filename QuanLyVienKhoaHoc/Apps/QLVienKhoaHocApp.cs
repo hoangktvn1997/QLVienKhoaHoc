c19a188c4d62642696cce74b5d94e3556cb24c30
Nguyễn Sỹ Hoàng <hoangktvn1997@gmail.com>
2017-12-08 10:58:22 +0700

    Coding Chọn loại nhân viên

---
+++ QuanLyVienKhoaHoc/Apps/QLVienKhoaHocApp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLyVienKhoaHoc.Enties;
+
+namespace QuanLyVienKhoaHoc.Apps
+{
+    class QLVienKhoaHocApp
+    {
+        //Định dạng list đối tượng
+        List<NhanVien> nhanViens = new List<NhanVien>();
+        
+        //Nhập list
+        public void NhapList()
+        {
+            int soLuongNhanVien;
+            int loaiNhanVien;
+            //Nhập số lượng
+            Console.Write("Nhập số lượng nhân viên: ");
+            soLuongNhanVien = int.Parse(Console.ReadLine());
+            //Nhập nhân viên
+            for (int i = 0; i < soLuongNhanVien; i++)
+            {
+                Console.WriteLine("\tThông tin nhân viên thứ {0}:", i + 1);
+                //Chọn loại nhân viên
+                Console.WriteLine("Chọn loại nhân viên cần nhập: ");
+                Console.WriteLine("\t1. Nhà khoa học");
+                Console.WriteLine("\t2. Nhà quản lý");
+                Console.WriteLine("\t3. Nhân viên phòng thí nghiệm");
+                Console.Write("\tChọn loại nhân viên: ");
+                //Lấy giá trị lựa chọn
+                loaiNhanVien = int.Parse(Console.ReadLine());
+                switch (loaiNhanVien)
+                {
+                    case 1;
+                        NhanVien = 
+                    default:
+                        break;
+                }
+            }
+            
+        }
+
+        //Xuất list
+        //Tính tổng lương
+    }
+}
